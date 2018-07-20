2ae381357b76c765e8bc9961e0ec418d0c355f09
JavAlisson <javalisson@gmail.com>
2018-07-19 22:07:34 -0300

    Adicionada a biblioteca de sons do p5js e a função preload para carregar sons e imagens

--- script.js
+++ script.js
@@ -4,16 +4,21 @@
  * 
  */
 
+// utilizada para carregar imagens, sons e fontes
+function preload() {
+    
+}
+
 // chamada no inicio do programa
 function setup() {
-	// cria o quadro, com dimensoes 900 x 400
-	createCanvas(900, 400);
+    // cria o quadro, com dimensoes 900 x 400
+    createCanvas(900, 400);
 }
 
 // chamada toda vez que o quadro for redesenhado
 // ou seja, a cada frame da animacao
 function draw() {
-	// pinta o fundo de preto
-	background(0, 0, 0);
+    // pinta o fundo de preto
+    background(0, 0, 0);
 
 }
